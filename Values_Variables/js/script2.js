c6cbcfb09b09477c7ef8b28e8140ea36352422c5
AlAlston <alalston@fullsail.edu>
2015-02-11 16:02:07 -0500

    Updated Values_Variables Folder (Defining Variables)
    updated script2.js file when following instructional video about
    Defining Variables

--- Values_Variables/js/script2.js
+++ Values_Variables/js/script2.js
@@ -1,4 +1,7 @@
-// Declaring Variables
+// Defining Variables
 
-var age;
+var age;// declaration
+age = 31;// definition
+//storage container = value
 
+var yearOfBirth = 1983; //declaring and defining on the same line
