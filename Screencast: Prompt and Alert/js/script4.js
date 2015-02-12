4607632a1f9ef6ad824a31d1921712a03958f69d
AlAlston <alalston@fullsail.edu>
2015-02-12 07:47:31 -0500

    Screencast: Prompt and Alert
    create Screencast: Prompt and Alert Directory with appropriate folders.
    followed video using script4.js file to learn out to get user input and
    to get that input to not only display in the console but as well in the
    prompts to the user.

---
+++ Screencast: Prompt and Alert/js/script4.js
@@ -0,0 +1,9 @@
+// Screencast: Prompt and Alert
+
+var width =  prompt ( "We are calculating the area of a rectangle. \nPlease Enter the width");
+var height = prompt ("Please enter the height.");
+var area = width * height;// calculate with that info
+//console.log(area);//prints it out to the console.
+
+var result = " The area of your rectangle is " + area + "sq feet";
+alert(result);
