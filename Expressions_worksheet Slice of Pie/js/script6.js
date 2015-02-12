bd81ec00b34e26e55965af99ad1c272b7c0ac673
AlAlston <alalston@fullsail.edu>
2015-02-12 12:49:21 -0500

    Expression_Worksheet Slice of Pie I
    Created folders and proper files script6.js file coded the calculation
    for how many slices person eaten at a party. Calculation will generate
    answer for any amount of people and pizza ordered.

---
+++ Expressions_worksheet Slice of Pie/js/script6.js
@@ -0,0 +1,30 @@
+// Adreinne Alston 11 February 2015 Expression_Worksheet Slice of Pie I
+
+
+//Slice of Pie part 1
+
+//A bunch of students are having a party and somebody ordered pizza. Create an expression that
+// calculates how much pizza each party goer will get at the party.  (Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
+//Given:
+
+//Number of slices per pizza
+//Number of people at the party
+//Number of pizzas ordered.
+
+    //Result Variables:
+
+    //Number of slices per person. (Can be a decimal or floating point).
+
+//Result to Print:
+
+    //“Each person ate X slices of pizza at the party.”
+
+
+var people = prompt ( "We are calculating the slices of pizza eaten per person. \nPlease Enter the number of people.");
+var pizzaOrdered = prompt ( "Enter the amount of Pizza ordered. ")
+//prompt allowing user to input the amount of pizza ordered
+var slicesPerPizza = 8; // 1 pizza equals 8 slices when divided equally
+var slicePerPerson = slicesPerPizza / people;
+
+var result = " Each person ate " + slicePerPerson + " slices of pizza at the party ";
+alert(result);
