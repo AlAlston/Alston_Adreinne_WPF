c8eb99daa8ecb15b792b88cd44fa1a586f25b1af
AlAlston <alalston@fullsail.edu>
2015-02-19 09:28:15 -0500

    Logical Operators
    changed var budget amount to 100 making the entire condition true

--- Screencast: Conditionals/js/script8.js
+++ Screencast: Conditionals/js/script8.js
@@ -1,11 +1,11 @@
 // Screencast: Logic Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
 var wonLottery = true;
 
 //if the iphone price is less our budget and if paycheck is over 300
-if(iPhonePrice < budget || wonLottery === 300) {
+if(iPhonePrice < budget || wonLottery === 300){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
