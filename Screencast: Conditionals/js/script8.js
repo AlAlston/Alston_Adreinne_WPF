ebe863d5abc8caf65c834b1219df5f6c53d6941b
AlAlston <alalston@fullsail.edu>
2015-02-19 09:19:14 -0500

    Logical Operators
    enter a second set of if else conditions and result to results printed
    into console

--- Screencast: Conditionals/js/script8.js
+++ Screencast: Conditionals/js/script8.js
@@ -4,9 +4,14 @@ var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
 
-//if the iphone price is less our budget
+//if the iphone price is less our budget and if paycheck is over 300
 if(iPhonePrice < budget) {
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
+}
+if(paycheck > 300) {
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone For you!");
 }
