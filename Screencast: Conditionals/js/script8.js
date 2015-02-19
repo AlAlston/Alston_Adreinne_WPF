3f494060e343cc2dc88a515d259237b2957ee82e
AlAlston <alalston@fullsail.edu>
2015-02-19 09:29:34 -0500

    Logical Operators
    remove the is this true or false variable

--- Screencast: Conditionals/js/script8.js
+++ Screencast: Conditionals/js/script8.js
@@ -5,7 +5,7 @@ var iPhonePrice = 199.99;
 var wonLottery = true;
 
 //if the iphone price is less our budget and if paycheck is over 300
-if(iPhonePrice < budget || wonLottery === 300){
+if(iPhonePrice < budget || wonLottery ){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
