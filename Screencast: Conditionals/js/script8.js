8f9a4d54ef121afc6c702c43ff9355d452367d2e
AlAlston <alalston@fullsail.edu>
2015-02-19 09:26:54 -0500

    Logical Operators
    changed conditions following video and variable from paycheck to won
    lottery

--- Screencast: Conditionals/js/script8.js
+++ Screencast: Conditionals/js/script8.js
@@ -2,10 +2,10 @@
 
 var budget = 300;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the iphone price is less our budget and if paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300) {
+if(iPhonePrice < budget || wonLottery === 300) {
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!");
