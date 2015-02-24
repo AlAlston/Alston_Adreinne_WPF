8bcd36178d4d130d78db5c792dcf0c2d3219e6f4
AlAlston <alalston@fullsail.edu>
2015-02-24 12:58:25 -0500

    Basic Functions
    Followed Basic Functions as set out in the video.

--- Functions/js/script10.js
+++ Functions/js/script10.js
@@ -1,3 +1,13 @@
-/**
- * Created by ALAlston on 2/23/15.
- */
+//Functions  - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+
+function calArea(){
+    var width = 20;
+    var height = 20;
+    var area = width * height;
+    console.log(area);
+}
